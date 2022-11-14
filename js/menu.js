77f50e47dcc7e13650dd0824153374f324a66636
MARIA VIEGAS <danielalmeida.da3@gmail.com>
2022-11-14 18:11:25 +0000

    1 commit

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,20 @@
 function menuShow() {
 
     let menuMobile = document.querySelector('.mobile-menu');
+    let mainBox = document.querySelector(".centerred");
 
     if (menuMobile.classList.contains('open')) {
 
-        menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "./img/svg/menu_white_36dp.svg";
 
+        menuMobile.classList.remove('open');
+        mainBox.style.display = "block";
+        document.querySelector('.icon').src = "./img/svg/menu_white_36dp.svg";
+        
     } else {
 
         menuMobile.classList.add('open');
         document.querySelector('.icon').src = "./img/svg/close_white_36dp.svg";
+        mainBox.style.display = "none";
 
     }
 

a72a2725041be87b531ae3782ad9c62361e1724d
MARIA VIEGAS <danielalmeida.da3@gmail.com>
2022-11-23 18:54:36 +0000

    5 commit

--- js/menu.js
+++ js/menu.js
@@ -1,46 +1,46 @@
-
-var menuMobile = document.querySelector('.mobile-menu');
-var mainBox = document.querySelector(".centerred");
-
-
-window.addEventListener("resize", ()=>{
-
-    mainBox.style.display = "block";
-
-    document.querySelector('#icon').classList.remove("fas", "fa-window-close");
-    document.querySelector('#icon').classList.add('fa-solid', 'fa-bars');
-
-    menuMobile.classList.remove('open');
-    
-
-});
-
-
-
-
-function menuShow() {
-
-    if (menuMobile.classList.contains('open')) {
-
-
-        menuMobile.classList.remove('open');
-        menuMobile.style.animation = "slide-down 0.5s forwards";
-
-        document.querySelector('#icon').classList.remove("fas", "fa-window-close");
-        document.querySelector('#icon').classList.add('fa-solid', 'fa-bars');
-        
-        mainBox.style.display = "block";
-        
-        
-    } else {
-
-        menuMobile.classList.add('open');
-
-        document.querySelector('#icon').classList.remove('fa-solid', 'fa-bars');
-        document.querySelector('#icon').classList.add("fas", "fa-window-close");
-
-        mainBox.style.display = "none";
-
-    }
-
+
+var menuMobile = document.querySelector('.mobile-menu');
+var mainBox = document.querySelector(".centerred");
+
+
+window.addEventListener("resize", ()=>{
+
+    mainBox.style.display = "block";
+
+    document.querySelector('#icon').classList.remove("fas", "fa-window-close");
+    document.querySelector('#icon').classList.add('fa-solid', 'fa-bars');
+
+    menuMobile.classList.remove('open');
+    
+
+});
+
+
+
+
+function menuShow() {
+
+    if (menuMobile.classList.contains('open')) {
+
+
+        menuMobile.classList.remove('open');
+        menuMobile.style.animation = "slide-down 0.5s forwards";
+
+        document.querySelector('#icon').classList.remove("fas", "fa-window-close");
+        document.querySelector('#icon').classList.add('fa-solid', 'fa-bars');
+        
+        mainBox.style.display = "block";
+        
+        
+    } else {
+
+        menuMobile.classList.add('open');
+
+        document.querySelector('#icon').classList.remove('fa-solid', 'fa-bars');
+        document.querySelector('#icon').classList.add("fas", "fa-window-close");
+
+        mainBox.style.display = "none";
+
+    }
+
 }
